23286e67254623ebe6834094b146543dae13ad8f
Kostiantyn Sotnikov <sotnik2804@gmail.com>
2025-08-20 20:28:45 +0200

    feat/add a model for telegram users

--- src/dao/models.py
+++ src/dao/models.py
@@ -1,4 +1,21 @@
 
-
+from sqlalchemy import Column, Integer, String, Boolean, Datetime
 from sqlalchemy.ext.declarative import declarative_base
 from datetime import datetime
+
+Base = declarative_base()
+
+
+class User(Base):
+    __tablename__ = "reposting_bot_users"
+    
+    telegram_id = Column(Integer, primary_key=True)
+    username = Column(String(50), nullable=True)
+    first_name = Column(String(50), nullable=False)
+    last_name = Column(String(50), nullable=True)
+    phone = Column(String(20), nullable=True)
+    is_admin = Column(Boolean, default=True)
+    registered_at = Column(DateTime, default=datetime.utcnow)
+    
+    def __repr__(self):
+        return f"<User(id={self.telegram_id}, username={self.username})>"
