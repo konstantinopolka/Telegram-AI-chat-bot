002e019163a25928f36b2985de5220b85481a736
Kostiantyn Sotnikov <sotnik2804@gmail.com>
2025-08-22 17:47:06 +0200

    Update the table

--- src/dao/models.py
+++ src/dao/models.py
@@ -1,6 +1,6 @@
 import os
 from dotenv import load_dotenv
-from sqlalchemy import Column, Integer, String, Boolean, DateTime, create_engine
+from sqlalchemy import Column, Integer, String, Boolean, DateTime, BigInteger, create_engine
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import declarative_base, sessionmaker
 from datetime import datetime, timezone
@@ -12,13 +12,13 @@ Base = declarative_base()
 class User(Base):
     __tablename__ = "reposting_bot_users"
     
-    telegram_id = Column(Integer, primary_key=True)
+    telegram_id = Column(BigInteger, primary_key=True)
     username = Column(String(50), nullable=True)
     first_name = Column(String(50), nullable=False)
     last_name = Column(String(50), nullable=True)
     phone = Column(String(20), nullable=True)
     is_admin = Column(Boolean, default=True)
-    registered_at = Column(DateTime, default=lambda: datetime.now(tz=timezone.utc))
+    registered_at = Column(DateTime(timezone=True), default=lambda: datetime.now(tz=timezone.utc))
     
     def __repr__(self):
         return f"<User(id={self.telegram_id}, username={self.username})>"
