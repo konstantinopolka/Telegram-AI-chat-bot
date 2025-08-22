ed5734c5c4b02e641449413066c126fd157d34ba
Kostiantyn Sotnikov <sotnik2804@gmail.com>
2025-08-22 11:28:42 +0200

    add async engine and connect it to the bot

--- src/dao/models.py
+++ src/dao/models.py
@@ -1,8 +1,10 @@
 import os
 from dotenv import load_dotenv
 from sqlalchemy import Column, Integer, String, Boolean, DateTime, create_engine
-from sqlalchemy.orm import declarative_base
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
+from sqlalchemy.orm import declarative_base, sessionmaker
 from datetime import datetime, timezone
+
 
 Base = declarative_base()
 
@@ -23,8 +25,12 @@ class User(Base):
     
 load_dotenv()
 DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///dev.db")
+ASYNC_DATABASE_URL = os.getenv("ASYNC_DATABASE_URL", "sqlite+aiosqlite:///dev.db")
     
+async_engine = create_async_engine(ASYNC_DATABASE_URL, echo=True)
 engine = create_engine(DATABASE_URL, echo=True)
+
+AsyncSessionLocal = sessionmaker(async_engine, class_ = AsyncSession, expire_on_commit=False)
 # Base.metadata.create_all(engine)
 
 
